4c8009a9286e544204efb863473cd4dc37a96ffb
makonheimak <114416982+makonheimak@users.noreply.github.com>
2025-05-29 17:08:03 +0300

    Add files via upload

---
+++ Go/Fundamentals.go
@@ -0,0 +1,24 @@
+/* package main
+
+import "fmt"
+
+func main() {
+	var age int = 19
+	var apartment int = 139
+
+	fmt.Println("add", age+apartment)
+	fmt.Println("a1", apartment-age)
+	fmt.Println("a2", age*apartment)
+	fmt.Println("a3", apartment/age)
+} */
+
+package main
+
+import "fmt"
+
+func main() {
+	var FirstName string = "Maxim"
+	var LastName string = "Yurash"
+
+	fmt.Println("FullName:", FirstName+"", LastName)
+}
